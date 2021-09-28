3a2a01dcbaf8ab6bd3568fbedfec510793032f5a
Michael Matos <matos2890@gmail.com>
2021-09-27 23:20:44 -0500

    added html code, css styling, added maps from google maps

--- js-final-boilerplates/trip-app/src/app/app.component.ts
+++ js-final-boilerplates/trip-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Central Team 2';
+  title = 'The Track your Trip App ';
 }
