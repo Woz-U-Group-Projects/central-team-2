f0552fc934b7f6f197b37b96d21e3f21cc1a22a7
Zak Kuczynski <zak_ku@yahoo.com>
2021-10-13 12:12:47 -0500

    Navbar 2.8

--- js-final-boilerplates/client-angular/src/app/app-routing.module.ts
+++ js-final-boilerplates/client-angular/src/app/app-routing.module.ts
@@ -1,8 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [  {
+  path: 'home',
+  component: MainPageComponent
+},
+// {
+//   path: 'login',
+//   component: LoginPageComponent
+// },
+// {
+//   path: 'trips',
+//   component: TripsPageComponent
+// },
+// {
+//   path: 'mytrips',
+//   component: MyTripsPageComponent
+// },
+// {
+//   path: 'pois',
+//   component: POIsPageComponent
+// },
+// {
+//   path: 'favorites',
+//   component: FavoritesPageComponent
+// },
+// {
+//   path: 'search',
+//   component: SearchPageComponent
+// },
+{
+  path: '',
+  redirectTo: '/home',
+  pathMatch: 'full'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
